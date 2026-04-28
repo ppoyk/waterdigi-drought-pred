ec160202550a314d1eeda3e5b11e1ae7f8ba1f84
ppoyk <138587496+ppoyk@users.noreply.github.com>
2026-04-28 13:05:47 +0300

    render all notebooks, add explanations

--- run_reproducibility.R
+++ run_reproducibility.R
@@ -1,7 +1,15 @@
+# This file is the reproducibility wrapper for the WaterDigitalization-course code project.
+# First, the environment consistency is ensured using `renv`.
+# Then, all the initerdependent data analysis Quarto notebooks are rendered in the correct sequence.
+
+# (Managing all this would have been better using Quarto's project capabilities)
+# (This could be implemented later)
+
 
 renv::restore()
+OUT_FORMAT <- "html"
 
-OUT_FORMAT <- "pdf"
+unlink("environment.RData") # start fresh
 
 cat("Downloading source data files...")
 quarto::quarto_render("SE1_data_access.qmd", OUT_FORMAT)
@@ -11,4 +19,17 @@ cat("Processing data...")
 quarto::quarto_render("SE2_data_processing.qmd", OUT_FORMAT)
 message("✅")
 
+cat("Calculating models...")
+quarto::quarto_render("SE3_modelling_steps.qmd", OUT_FORMAT)
+message("✅")
+
+cat("Creating visualizations and the data story...")
+quarto::quarto_render("SE4_results_viz.qmd", OUT_FORMAT)
+message("✅")
+
+cat("Building README...")
+quarto::quarto_render("README.md", OUT_FORMAT)
+message("✅")
+
 message("All output analysis notebooks have been created ✅")
+#unlink("environment.RData") # clean env when finishes
