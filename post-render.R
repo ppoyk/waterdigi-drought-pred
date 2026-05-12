5942a7557c8386de298e1c622044ec203697f425
ppoyk <138587496+ppoyk@users.noreply.github.com>
2026-05-12 16:15:50 +0300

    fix rendering

--- post-render.R
+++ post-render.R
@@ -2,9 +2,10 @@
 # File to perform some maintenance tasks after a Quarto project render
 
 # Delete the global R-env shared across .qmd s
-if (fs::file_exists("environment.RData")) {
-  fs::file_delete("environment.RData")
-}
+# if (fs::file_exists("environment.RData")) {
+#   fs::file_delete("environment.RData")
+# }
+# Wouldn't work if quarto freezing is used on the project level
 
 # Copy the rendered (github-flavored-md) README to project root
-fs::file_copy("docs/README.md", "README.md")
+fs::file_copy("docs/README.md", "README.md", overwrite = TRUE)
